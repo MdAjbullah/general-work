287f461d95901fe1e5c347a3ff53b21bc5c864a7
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2024-02-04 15:56:32 +0530

    promisses

---
+++ promisses.js
@@ -0,0 +1,23 @@
+// promiss is a specila object in JS that represents a value that may be available now, or in the future, or never.
+//  syntax
+// function createPromise() {
+//   return new Promise((resolve, reject) => {
+  
+//   });
+// }
+
+
+function createPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Promise resolved');
+    }, 2000);
+  });
+}
+
+console.log('start');
+let x= createPromise();
+console.log(" gote a new promise");
+x.then( function f(){
+  console.log("promise resolved");
+});
